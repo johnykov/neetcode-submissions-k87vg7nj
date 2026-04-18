958f792da52258bbcb116c677a0e7551a1185c82
Jan Kowalski <hanskoff@gmail.com>
2026-04-18 07:20:14 +0200

    Add: topologicalSort - submission-2

---
+++ Data Structures & Algorithms/topologicalSort/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        for src, dst in edges:
+            adj[src].append(dst)
+
+        visit = set()
+        visiting = set()
+        topSort = []
+
+        def dfs(crs):
+            if crs in visit:
+                return True
+            if crs in visiting:
+                return False
+            visiting.add(crs)
+
+            for neighbor in adj[crs]:
+                if not dfs(neighbor):
+                    return False
+            visiting.remove(crs)
+            visit.add(crs)
+            topSort.append(crs)
+            return True
+
+        for i in range(n):
+            if not dfs(i):
+                return []
+        if len(topSort) != n:
+            return []
+        topSort.reverse()
+        
+        return topSort
