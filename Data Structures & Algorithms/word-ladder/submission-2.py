2e7f7d224e424fe6fc6aeb8ca0dc9c845de4b385
Jan Kowalski <hanskoff@gmail.com>
2026-05-30 06:40:00 +0200

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        wordList = list(set(wordList + [beginWord]))
+        graph = defaultdict(list)
+
+        for i in range(len(wordList)):
+            for j in range(i + 1, len(wordList)):
+                if sum([a != b for a,b in zip(wordList[i], wordList[j])]) == 1:
+                    graph[wordList[i]].append(wordList[j])
+                    graph[wordList[j]].append(wordList[i])
+
+        visit = {beginWord}
+        q = deque([(beginWord, 1)])
+
+        while q:
+            node, length = q.popleft()
+            if node == endWord:
+                return length
+            
+            for nei in graph[node]:
+                if nei not in visit:
+                    visit.add(nei)
+                    q.append((nei, length + 1))
+
+        return 0
