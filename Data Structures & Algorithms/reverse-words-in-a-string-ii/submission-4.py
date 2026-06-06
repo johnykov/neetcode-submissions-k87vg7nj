2756a98ba88771607107cf5cdac3c015ccf003d8
Jan Kowalski <hanskoff@gmail.com>
2026-06-06 07:49:11 +0200

    Add: reverse-words-in-a-string-ii - submission-4

---
+++ Data Structures & Algorithms/reverse-words-in-a-string-ii/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse(l, r):
+            while l < r:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+    
+        reverse(0, len(s) - 1)
+
+        last = -1
+        for i in range(len(s)):
+            if s[i] == " ":
+                reverse(last + 1, i - 1)
+                last = i
+
+        reverse(last + 1, len(s) - 1)
+        
